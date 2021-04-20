e927f97475de05a45d468f4aa8404de3f3ec6289
Xiang Zhang <xz95@uw.edu>
2021-04-20 01:22:27 -0700

    add port 3000

--- server.js
+++ server.js
@@ -30,4 +30,4 @@ require("./controllers/quizzes-controller")(app)
 require("./controllers/questions-controller")(app)
 require("./controllers/quiz-attempts-controller")(app)
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || 3000)
